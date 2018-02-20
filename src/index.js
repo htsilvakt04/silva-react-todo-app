b3ff5d8cdf798b459b67542ecddacdfb87c7844e
silva <htsilvakt04@gmail.com>
2018-02-20 20:26:44 +0700

    prepare handleDelete todo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ class UserInputForm extends React.Component {
     render () {
         return (
             <form onSubmit={this.props.onSubmit}>
-                <input type="text" onChange={this.props.onChange} name="todoText" placeholder="Add new item..." value={this.props.todoText} />
+                <input type="text" onChange={this.props.onChange}
+                       name="todoText" placeholder="Add new item..." value={this.props.todoText}
+                />
                 <button type="submit" name="submit">Add</button>
             </form>
         );
@@ -14,7 +16,7 @@ class UserInputForm extends React.Component {
 class TodoItem extends React.Component {
     render () {
         return (
-            <li>{this.props.text}</li>
+            <li onClick={this.props.onClick}>{this.props.text}</li>
         )
     }
 }
@@ -26,7 +28,7 @@ class ListTodo extends React.Component {
     renderEachItem () {
         const itemList = [];
         this.props.todos.forEach((item, index) => {
-            itemList.push(<TodoItem key={index} text={item.text}/>)
+            itemList.push(<TodoItem onClick={this.props.onClick} key={index} text={item.text}/>)
         });
         return itemList;
     }
@@ -48,23 +50,32 @@ class TodoTable extends React.Component {
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
     }
+    handleDelete () {
 
+    }
     handleChange (event) {
        const userText = event.target.value;
        this.setState({todoText: userText});
     }
-
     handleSubmit (event) {
         event.preventDefault();
-        alert('submit');
+        const todoToSave = this.state.todoText;
+        this.props.todos.push(
+            {text: todoToSave}
+        );
+        this.setState((prevState, props) => {
+           return {todoText: ''};
+        });
+
     }
 
     render () {
         return (
             <div id="todo-table">
                 <UserInputForm onSubmit={this.handleSubmit} onChange={this.handleChange} todoText={this.state.todoText}/>
-                <ListTodo todos={this.props.todos}/>
+                <ListTodo onClick={this.handleDelete} todos={this.props.todos}/>
             </div>
         );
     }
