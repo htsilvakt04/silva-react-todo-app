3e860abcaa1d3257f49e56a11b47dbd0a6b9abb4
silva <htsilvakt04@gmail.com>
2018-02-20 19:28:45 +0700

    ready implement save action

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 class UserInputForm extends React.Component {
     render () {
         return (
-            <form>
-                <input type="text" name="todoText" placeholder="Add new item..." value={this.props.todoText} />
+            <form onSubmit={this.props.onSubmit}>
+                <input type="text" onChange={this.props.onChange} name="todoText" placeholder="Add new item..." value={this.props.todoText} />
                 <button type="submit" name="submit">Add</button>
             </form>
         );
@@ -45,13 +45,25 @@ class TodoTable extends React.Component {
         super(props);
         this.state = {
             todoText: ''
-        }
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange (event) {
+       const userText = event.target.value;
+       this.setState({todoText: userText});
+    }
+
+    handleSubmit (event) {
+        event.preventDefault();
+        alert('submit');
     }
 
     render () {
         return (
             <div id="todo-table">
-                <UserInputForm todoText={this.state.todoText}/>
+                <UserInputForm onSubmit={this.handleSubmit} onChange={this.handleChange} todoText={this.state.todoText}/>
                 <ListTodo todos={this.props.todos}/>
             </div>
         );
